c4f3664abe4b163d42740f39618b95864e10b51d
WinsleyJ <winsleyjvdh@gmail.com>
2022-08-04 22:56:18 +0200

    first commit

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,9 @@
+<script setup>
+// TODO: Create a awesome 404 page
+</script>
+<template>
+    <div>
+        <h1>Page not found</h1>
+        <p>The page you are looking for does not exist</p>
+    </div>
+</template>
